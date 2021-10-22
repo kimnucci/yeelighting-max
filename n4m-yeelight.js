285fcf5b931bb6018e66abf630737939f98000a9
Varsha Iyengar <ivarsha@amazon.com>
2021-10-21 21:53:02 -0700

    I added a try block and it is not breaking - weird

--- n4m-yeelight.js
+++ n4m-yeelight.js
@@ -9,7 +9,7 @@ let bulb = null
  * Respond to the `connect` message from Max
  */
 Max.addHandler('connect', (ip) => {
-	/** If bulb hasn't been instantiated */
+	/** If bulb hasn'e been instantiated */
 	if (!bulb) {
 
 		bulb = new Bulb(ip)
@@ -49,6 +49,7 @@ Max.addHandler('disconnect', () => {
  * Respond to the `on` message from Max
  */
 Max.addHandler('on', () => {
+		console.log("hi")
 	if (bulb) {
 		bulb.onn()
 	}
@@ -65,18 +66,42 @@ Max.addHandler('off', () => {
 
 /**
  * Respond to the `toggle` message from Max
- */
+*/
 Max.addHandler('toggle', () => {
+	try{
+		
 	if (bulb) {
 		bulb.toggle()
 	}
+	} catch(err){
+		Max.post(err)
+		}
 })
+/**
+
+const { toggle } = require('yeelight.io');
+ 
+toggle('192.168.86.209', (err) => {
+  if (err) {
+    console.error(`error [${err.message}] occured on 192.168.10.227`);
+  } else {
+    console.log('toggle 192.168.1.227 success');
+  }
+});
+
+if (bulb){
+	bulb.toggle()
+	}
+	*/
+
 
 /**
  * Respond to the `brightness` message from Max
  */
 Max.addHandler('brightness', (value) => {
+	Max.post(value)
 	if (bulb) {
+		Max.post(value)
 		bulb.brightness(value)
 	}
 })
@@ -85,6 +110,11 @@ Max.addHandler('brightness', (value) => {
  * Respond to the `color` message from Max
  */
 Max.addHandler('color', (r, g, b) => {
+	Max.post('r:'+ r)
+		Max.post('g:'+ g)
+			Max.post('b:'+ b)
+	Max.post(g)
+	Max.post(b)
 	if (bulb) {
 		bulb.color(r, g, b)
 	}
